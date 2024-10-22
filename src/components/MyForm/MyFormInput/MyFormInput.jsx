7ac04e1a78bdb832c09962f87e73e2c70c574321
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-10-22 16:13:23 +0600

    input update

--- src/components/MyForm/MyFormInput/MyFormInput.jsx
+++ src/components/MyForm/MyFormInput/MyFormInput.jsx
@@ -1,7 +1,7 @@
 import { Form, Input } from "antd";
 import { Controller, useFormContext } from "react-hook-form";
 
-const MyFormInput = ({ type, name, label }) => {
+const MyFormInput = ({ type, name, label, inputClassName }) => {
   const { control } = useFormContext();
   return (
     <div>
@@ -11,11 +11,17 @@ const MyFormInput = ({ type, name, label }) => {
         rules={{
           required: true,
         }}
-        render={({ field, fieldState: { error }}) => (
+        render={({ field, fieldState: { error } }) => (
           <div className="flex flex-col justify-center gap-1">
             <p className="ps-1">{label}</p>
             <Form.Item style={{ marginBottom: "0px" }}>
-              <Input {...field} type={type} id={name} size="large" />
+              <Input
+                {...field}
+                type={type}
+                id={name}
+                size="large"
+                className={cn("focus:!border-[#7F56D9] hover:!border-[#7F56D9]", inputClassName)}
+              />
             </Form.Item>
             {error && <small style={{ color: "red" }}>{error.message}</small>}
           </div>
