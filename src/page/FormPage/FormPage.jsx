140da87d12f6d145bb1d0dc73de475074b1edfbc
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-10-22 16:37:37 +0600

    error update

--- src/page/FormPage/FormPage.jsx
+++ src/page/FormPage/FormPage.jsx
@@ -8,77 +8,115 @@ import MyFormPasswordInput from "../../components/MyForm/MyFormPasswordInput/MyF
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-
 export const validationSchema = z.object({
-    name: z.string()
+  name: z
+    .string({
+      required_error: "This field is required",
+    })
     .min(2, "Name must be at least 2 characters long")
     .max(50, "Name cannot exceed 50 characters")
-    .regex(/^[A-Za-z\s\-,'.]+$/, "Name can only contain letters, spaces, hyphens, commas, apostrophes, and dots")
-    .optional(), // Optional if not required
-    email: z.string()
     .regex(
-        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-        "Please enter a valid email address"
-    ),
-    password: z.string().min(6, "Must be at least 6 characters"),
+      /^[A-Za-z\s\-,'.]+$/,
+      "Name can only contain letters, spaces, hyphens, commas, apostrophes, and dots"
+    )
+    .optional(), // Optional if not required
+  email: z
+    .string({
+      required_error: "This field is required",
+    })
+    .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please enter a valid email address"),
+  password: z
+    .string({
+      required_error: "This field is required",
+    })
+    .min(6, "Must be at least 6 characters"),
 
-    phone: z.string()
+  phone: z
+    .string({
+      required_error: "This field is required",
+    })
     .min(11, "Phone number must be 11 digits long")
     .max(11, "Phone number must be 11 digits long")
-    .regex(/^0\d{10}$/, "Please enter a valid 11-digit phone number starting with 0")
-    .refine((value) => !!value, { message: "Phone number is required" }) // Ensure the value is not empty
+    .regex(
+      /^0\d{10}$/,
+      "Please enter a valid 11-digit phone number starting with 0"
+    )
+    .refine((value) => !!value, { message: "Phone number is required" }), // Ensure the value is not empty
+});
 
-  });
-  
 const FormPage = () => {
   const handleSubmit = (data) => {
     console.log(data);
   };
 
   const defaultValues = {
-    name: '2026010016',
-    password: 'student123',
+    name: "2026010016",
+    password: "student123",
   };
 
-  const option=[
+  const option = [
     {
-      value: 'jack',
-      label: 'Jack',
+      value: "jack",
+      label: "Jack",
     },
     {
-      value: 'lucy',
-      label: 'Lucy',
+      value: "lucy",
+      label: "Lucy",
     },
     {
-      value: 'Yiminghe',
-      label: 'yiminghe',
+      value: "Yiminghe",
+      label: "yiminghe",
     },
     {
-      value: 'disabled',
-      label: 'Disabled',
+      value: "disabled",
+      label: "Disabled",
       disabled: true,
     },
-  ]
-  const [courseId, setCourseId] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-//   console.log(courseId, password, confirmPassword);
+  ];
+  const [courseId, setCourseId] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  //   console.log(courseId, password, confirmPassword);
 
-  
   return (
     <div style={{ padding: 20 }}>
-        
       <h1>My Form Example</h1>
-      <MyFormWrapper className={"flex flex-col gap-2"} onSubmit={handleSubmit} resolver={zodResolver(validationSchema)} defaultValues={defaultValues}>
+      <MyFormWrapper
+        className={"flex flex-col gap-2"}
+        onSubmit={handleSubmit}
+        resolver={zodResolver(validationSchema)}
+        defaultValues={defaultValues}
+      >
         <MyFormInput name={"name"} label={"Name"} type={"text"} />
         <MyFormInput name={"email"} label={"Email"} type={"email"} />
         <MyFormInput name={"phone"} label={"phone"} type={"number"} />
-        <MyFormPasswordInput name={"password"} label={"password"} type={"password"} onValueChange={setPassword}/>
-        <MyFormPasswordInput name={"c_password"} label={"c_password"} type={"password"} matchWith={"password"} onValueChange={setConfirmPassword}/>
-        <MyFormSelect name={"select"} label={"select"} options={option}/>
-        <MyFormSelectWithWatch name={"select - 2"} options={option} label={"select"} onValueChange={setCourseId} />
-        <MyFormTimePicker name={"timepicker"} label={"Time Picker"}  />
-        <button className="border w-min py-2 px-3 rounded bg-blue-400 text-white mt-2" type="submit">Submit</button>
+        <MyFormPasswordInput
+          name={"password"}
+          label={"password"}
+          type={"password"}
+          onValueChange={setPassword}
+        />
+        <MyFormPasswordInput
+          name={"c_password"}
+          label={"c_password"}
+          type={"password"}
+          matchWith={"password"}
+          onValueChange={setConfirmPassword}
+        />
+        <MyFormSelect name={"select"} label={"select"} options={option} />
+        <MyFormSelectWithWatch
+          name={"select - 2"}
+          options={option}
+          label={"select"}
+          onValueChange={setCourseId}
+        />
+        <MyFormTimePicker name={"timepicker"} label={"Time Picker"} />
+        <button
+          className="border w-min py-2 px-3 rounded bg-blue-400 text-white mt-2"
+          type="submit"
+        >
+          Submit
+        </button>
       </MyFormWrapper>
     </div>
   );
